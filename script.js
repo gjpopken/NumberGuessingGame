5e62a597f475bed910814cbcab022ee99b8c5d2f
Gavin Popken <gavinpopkenart@gmail.com>
2023-11-15 15:08:03 -0600

    the game works, and now am trying to add the ability to hit return and it guess the number

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 let randomNumber = Math.floor(Math.random()*101);
+let attempts = 0;
 
-console.log(randomNumber);
+//console.log(randomNumber);
 
 
 let guess_button = document.getElementById("guess_button");
-console.log(guess_button);
-
-
 guess_button.addEventListener('click', checkAnswer);
+/*guess_button.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter' || event.key === 'Return'){
+        //checkAnswer();
+        event.preventDefault();
+        document.getElementById("guess_button").click();
+    }
+});*/
 
 
 
 function checkAnswer() {
     let theGuess = document.getElementById('the_guess').value;
-    if (theGuess.value === randomNumber) {
-        console.log('You win!');
-    } else if (theGuess.value > randomNumber) {
+    //console.log(theGuess);
+    if (theGuess == randomNumber) {
+        console.log('You win!\nNumber of attempts:', attempts);
+        attempts++;
+        const congrats = 'Congrats! You win! \nAttempts: ' + attempts;
+        document.getElementById('response').innerHTML = congrats;
+    } else if (theGuess > randomNumber) {
         console.log('too high');
-        theGuess.value = '';
-    } else if (theGuess.value < randomNumber) {
+        document.getElementById('the_guess').value = '';
+        attempts++;
+        document.getElementById('response').innerHTML = 'Too high!';
+    } else if (theGuess < randomNumber && theGuess !== '') {
         console.log('too low');
-        theGuess.value = '';
+        document.getElementById('the_guess').value = '';
+        attempts++;
+        document.getElementById('response').innerHTML = 'Too low!';
     } else {
         console.log('Invalid answer')
-        theGuess.value = '';
+        document.getElementById('the_guess').value = '';
+        document.getElementById('response').innerHTML = 'Invalid Answer!';
     }
+
 }
