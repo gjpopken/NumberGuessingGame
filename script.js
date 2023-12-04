8443df7174ce20adb41e27d36c3da05e5616eece
Gavin Popken <gavinpopkenart@gmail.com>
2023-12-04 13:57:23 -0600

    updated to include more of what ive learned about events and functions. Refactored

--- script.js
+++ script.js
@@ -1,43 +1,32 @@
 let randomNumber = Math.floor(Math.random()*101);
 let attempts = 0;
+console.log(randomNumber);
 
-//console.log(randomNumber);
-
-
-let guess_button = document.getElementById("guess_button");
-guess_button.addEventListener('click', checkAnswer);
-/*guess_button.addEventListener('keypress', function(event) {
-    if (event.key === 'Enter' || event.key === 'Return'){
-        //checkAnswer();
-        event.preventDefault();
-        document.getElementById("guess_button").click();
-    }
-});*/
-
-
-
-function checkAnswer() {
+function checkAnswer(event) {
+    event.preventDefault()
     let theGuess = document.getElementById('the_guess').value;
     //console.log(theGuess);
     if (theGuess == randomNumber) {
-        console.log('You win!\nNumber of attempts:', attempts);
-        attempts++;
-        const congrats = 'Congrats! You win! \nAttempts: ' + attempts;
-        document.getElementById('response').innerHTML = congrats;
+        //console.log('You win!\nNumber of attempts:', attempts, );
+        const congrats = 
+        `Congrats! You win! 
+        Attempts: ${attempts}
+        The secret number was: ${randomNumber}`;
+        display(congrats)
     } else if (theGuess > randomNumber) {
-        console.log('too high');
-        document.getElementById('the_guess').value = '';
-        attempts++;
-        document.getElementById('response').innerHTML = 'Too high!';
+        //console.log('too high');
+        display('Too high!')
     } else if (theGuess < randomNumber && theGuess !== '') {
-        console.log('too low');
-        document.getElementById('the_guess').value = '';
-        attempts++;
-        document.getElementById('response').innerHTML = 'Too low!';
-    } else {
-        console.log('Invalid answer')
-        document.getElementById('the_guess').value = '';
-        document.getElementById('response').innerHTML = 'Invalid Answer!';
+        //console.log('too low');
+        display('Too low!')
     }
 
+}
+
+
+function display(message) {
+    //console.log('inside display');
+    document.getElementById('the_guess').value = ''
+    document.getElementById('response').innerHTML = message
+    attempts++
 }
